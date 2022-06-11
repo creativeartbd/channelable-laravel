f56de61892d806f6f96e89663652c12170dd19d0
Md Morshadun Nur <13203016@iubat.edu>
2022-06-11 11:34:52 +0600

    models and migrations created from old project

---
+++ app/Models/MysqlServer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MysqlServer extends Model
+{
+    use HasFactory;
+}
