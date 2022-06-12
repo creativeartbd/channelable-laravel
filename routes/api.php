226eb8e5dc97c51ff9cda22c6598d5da2faa9d71
Md Morshadun Nur <13203016@iubat.edu>
2022-06-13 00:16:30 +0600

    Auth route declared and set controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login')->name('login');
+    Route::post('register', 'register')->name('register');
+    Route::post('logout', 'logout')->name('logout');
+    Route::post('refresh', 'refresh');
+    Route::get('me', 'me')->name('profile');
+
+});
+
+
+Route::group(['middleware' => 'jwt.verify'], function ($router) {
+    Route::post('logout', 'Auth\AuthController@logout');
+//    Route::post('refresh', 'Auth\AuthController@refresh');
+//    Route::post('me', 'Auth\AuthController@me');
+//    Route::post('user/lang', 'Auth\AuthController@updateLang');
+//    Route::post('change/password', 'Auth\AuthController@changePassword');
+//    Route::post('user/update', 'Auth\AuthController@update');
+
+
+
+
+
+
 });
